e71f2585f51fc731cb9c243d169e859a26af547a
luisantoniococa <luicocurd@gmail.com>
2019-10-30 18:34:39 -0500

    we are still ahead with the projec and we added and identified the candidates

---
+++ Poll_Analysis/Poll_data_analysis.py
@@ -0,0 +1,37 @@
+import os 
+import csv
+
+#creating a variable to contain the data to be executed
+poll_data = os.path.join('..','Poll_Analysis','PyPoll_election_data.csv')
+# define the functions for different part of the Analysis
+def dictionary(data):
+    candidate = str(data[2])
+    table = {}
+with open (poll_data, 'r') as csvfile:
+    poll = csv.reader(csvfile, delimiter = ',')
+    next (poll)
+    candidates = []
+    count = 0
+    for row in poll:
+        count += 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+    votetotal =[]    
+    for person in candidates:
+        print(person)
+        votetotal.append(int(0))
+        
+    print (f'Total votes {count}')
+    print (votetotal)
+    print (candidates)
+    for row in poll:
+        for i in range(len(candidates)):
+            if str(candidates[i-1]) == str(row[2]):
+                votetotal[i-1] = votetotal[i-1] + 1
+                print (row[2])
+    print (votetotal)
+    votecounter = {
+        person : counter
+
+    }       
+        
