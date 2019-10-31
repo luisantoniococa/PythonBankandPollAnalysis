e7cfa80558e045a4722b326084c7b76e657e09ae
luisantoniococa <luicocurd@gmail.com>
2019-10-30 23:38:21 -0500

    we added all the comments to explain the code better and finished the final clean output

--- Poll_Analysis/Poll_data_analysis.py
+++ Poll_Analysis/Poll_data_analysis.py
@@ -3,27 +3,36 @@ import csv
 
 #creating a variable to contain the data to be executed
 poll_data = os.path.join('..','Poll_Analysis','PyPoll_election_data.csv')
-# define the functions for different part of the Analysis
-def dictionary(data):
-    candidate = str(data[2])
-    table = {}
+#opening the data as read for csv files
 with open (poll_data, 'r') as csvfile:
+    #setting the delimiter for a comma
     poll = csv.reader(csvfile, delimiter = ',')
+    #using next to ignore the first row with headers
     next (poll)
+    #creating an empty dictionary where the candidates and the votes are going to be stored
     candidates = {}
-    count = 0
+    #a variable to count all the votes
+    vote_count = 0
+    #the iteration for every row in the csv
     for row in poll:
-        count += 1
+        vote_count += 1
+        #we are going to store in the dictionary any name that is not repetitive and when ever the name is founded for the first time it will create the first vote
         if row[2] not in candidates.keys():
-            candidates[row[2]] = 0    
+            candidates[row[2]] = 1    
         if row[2] in candidates.keys():
             candidates[row[2]] += 1
-    winner = [0, 'person']
+    #creating an empty array with the winner name and the ammount of votes
+    winner = ['person',0]
+    #printing the results of the vote count
+    print(f'Election Results \n---------------------- \nTotal Votes: {vote_count} \n-----------------------------')
+    #using the for statement in orther to find the winner
     for person in candidates:
-        if candidates[person] > winner[0]:
-             
-            print (f' {person} : {candidates[person]} {(candidates[person]/count)*100} %' )
-
+        if candidates[person] > winner[1]:
+            winner[0]= person
+            winner[1]= candidates[person]
+        #printing all the results 
+        print (f' {person} : {round((candidates[person]/vote_count)*100, 4)} % ({candidates[person]}) Votes ' )
+    print(f'---------------------------\nWinner: {winner[0]} \n---------------------')
     #votetotal =[]    
     # for person in candidates:
     #     print(person)
