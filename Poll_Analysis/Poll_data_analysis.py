5e16c08dd16b004c9cda6331a2da9c9ceb496a43
luisantoniococa <luicocurd@gmail.com>
2019-10-30 21:55:30 -0500

    we changed the code to be more efficient and use dictionary

--- Poll_Analysis/Poll_data_analysis.py
+++ Poll_Analysis/Poll_data_analysis.py
@@ -10,28 +10,36 @@ def dictionary(data):
 with open (poll_data, 'r') as csvfile:
     poll = csv.reader(csvfile, delimiter = ',')
     next (poll)
-    candidates = []
+    candidates = {}
     count = 0
     for row in poll:
         count += 1
-        if row[2] not in candidates:
-            candidates.append(row[2])
-    votetotal =[]    
+        if row[2] not in candidates.keys():
+            candidates[row[2]] = 0    
+        if row[2] in candidates.keys():
+            candidates[row[2]] += 1
+    winner = [0, 'person']
     for person in candidates:
-        print(person)
-        votetotal.append(int(0))
-        
-    print (f'Total votes {count}')
-    print (votetotal)
-    print (candidates)
-    for row in poll:
-        for i in range(len(candidates)):
-            if str(candidates[i-1]) == str(row[2]):
-                votetotal[i-1] = votetotal[i-1] + 1
-                print (row[2])
-    print (votetotal)
-    votecounter = {
-        person : counter
+        if candidates[person] > winner[0]:
+             
+            print (f' {person} : {candidates[person]} {(candidates[person]/count)*100} %' )
 
-    }       
+    #votetotal =[]    
+    # for person in candidates:
+    #     print(person)
+    #     votetotal.append(int(0))
+        
+    # print (f'Total votes {count}')
+    # print (votetotal)
+    # print (candidates)
+    # for row in poll:
+    #     for i in range(len(candidates)):
+    #         if str(candidates[i-1]) == str(row[2]):
+    #             votetotal[i-1] = votetotal[i-1] + 1
+    #             print (row[2])
+    # print (votetotal)
+    # votecounter = {
+    #     person : counter
+
+    # }       
         
