aa8ed3c756e73beee7024d47f1369ac624e99193
luisantoniococa <luicocurd@gmail.com>
2019-11-01 13:19:01 -0500

    I run over some probles for the average change and while trying to solved I broke the code, then I had to start from 0 and arrived to a much cleaner resultalso got better results and added comments for the easier read

--- Bank_Analysis/Bank_Profit_and_Losses_by_month.py
+++ Bank_Analysis/Bank_Profit_and_Losses_by_month.py
@@ -2,47 +2,47 @@ import os
 import csv 
 #creating a variable to contain the data to be executed
 bank_data_csv = os.path.join('..','Bank_Analysis','PyBank_Resources_budget_data.csv')
-# define the functions for different part of the Analysis
-def finantial_analysis(bank_dataset):
-    month = str(bank_dataset[0])
-    change = int(bank_dataset[1])
-    absolutechange = int(abs(change))
-    # ask in class about using len to obtain the values of the column months
-    #print (len(month))
-    # net_total = 0
-    # for row in bank_dataset:
-    #     net_total += absolutechange
-    #     print (absolutechange)
-    #print(f'the net total is {net_total}')
-    #return net_total
-#read the csvfile
-def average(numbers):
-    length = len(numbers)
-    total = 0.00
-    for number in numbers:
-        total += number
-    return total / length
-
+# using open to read the csv file and execute the analysis
 with open(bank_data_csv, 'r') as csvfile:
     #split the data with commas
     csvreader = csv.reader(csvfile, delimiter = ',')
-    #defining the variable for counting months
+    #defining the variable for counting months, the net total
     counter = 0
     net_total = 0
-    greater_change=[0.00, "date"]
-    lowest_change=[0.00, "date"]
+    #creating arrays to save the data and the amount of the max and min changes 
+    greater_change=["date",0.00]
+    lowest_change=["date",0.00]
+    #skip the top row with the headers
     next(csvreader)
     print ("\n \n Finantial Analysis \n-----------------------------")
-    #counting the amount of months
+    #creating a 2d array to save the information for better iteration 
+    array=[]
+    array.append([])
+    array.append([])
     for row in csvreader:
-        counter += 1 
-        net_total += int(row[1])
-        if int(row[1])>greater_change[0]:
-            greater_change[0] = int(row[1])
-            greater_change[1] = str(row[0])
-        if int(row[1]) < lowest_change[0]:
-            lowest_change[0] = int(row[1])
-            lowest_change[1] = str(row[0])
-
-    print(f'Months: {counter} \nNet total: {net_total} \nAverage: {net_total/counter}')
-    print (f'Greatest Change:{greater_change[1]} {greater_change[0]} \nLowest Change: {lowest_change[1]} {lowest_change[0]}')
+        #saving the information from the csv in the array we created earlier
+        array[0].append(row[0])
+        array[1].append(int(row[1]))
+        #this condition let us identify the mas and min change and saved in corresponding arrays
+        if array[1][counter]>greater_change[1]:
+            greater_change[1] = array[1][counter]
+            greater_change[0] = array[0][counter]
+        if array[1][counter]<lowest_change[1]:
+            lowest_change[1] = array[1][counter]
+            lowest_change[0] = array[0][counter]
+        #sum of net total and adding a counter for the array iteration
+        net_total +=array[1][counter]
+        counter += 1
+    #creating a variables for the total average change iteration and sum 
+    change=0
+    i=1
+    #while loop to get the sum and then create the average
+    while i is not counter :
+        change = change + (array[1][i]-array[1][i-1])
+        i+=1
+        #this condition makes sure the get out of the loop as soon as we are out of range of the array
+        if i is (counter):
+            break
+    #printing the results and doing some basic formating
+    print(f'Months: {counter} \nNet total: {net_total} \nAverage: {change/counter}')
+    print (f'Greatest Change:{greater_change[0]} {greater_change[1]} \nLowest Change: {lowest_change[0]} {lowest_change[1]}')
